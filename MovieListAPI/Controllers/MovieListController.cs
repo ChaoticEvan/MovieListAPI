8679b2dabce87b52bee78089c6f58cac495069e4
Evan Voordeckers <evan.voordeckers@yahoo.com>
2023-01-20 16:03:29 -0500

    Fixed error of async return type

--- MovieListAPI/Controllers/MovieListController.cs
+++ MovieListAPI/Controllers/MovieListController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
 
 namespace MovieListAPI.Controllers
 {
@@ -8,15 +9,35 @@ namespace MovieListAPI.Controllers
     {
         private readonly ILogger<MovieListController> _logger;
 
-        public MovieListController(ILogger<MovieListController> logger)
+        private IHttpClientFactory _httpClientFactory;
+
+        private const string COUNTRY_CODE = "US";
+
+        private const string LANGUAGE_CODE = "en";
+
+        public MovieListController(ILogger<MovieListController> logger, IHttpClientFactory httpClientFactory)
         {
             _logger = logger;
+            _httpClientFactory = httpClientFactory;
         }
 
         [HttpGet(Name = "GetMovieList")]
-        public IEnumerable<Movie> Get()
+        public async Task<IEnumerable<Movie>> Get()
         {
-            return new List<Movie>();
+            var httpRequestMessage = new HttpRequestMessage(
+                HttpMethod.Get,
+                "https://api.themoviedb.org/3/movie/now_playing?")
+            {
+                Headers =
+                {
+                    { HeaderNames.Accept, "application/vnd.github.v3+json" },
+                    { HeaderNames.UserAgent, "HttpRequestsSample" }
+                }
+            };
+
+            var httpClient = _httpClientFactory.CreateClient();
+            var response = await httpClient.SendAsync(httpRequestMessage);
+            return null;
         }
     }
 }
