e029d7a817e43673146393d0ce09191fb788685b
Evan Voordeckers <evan.voordeckers@yahoo.com>
2023-01-24 18:25:29 -0500

    Fleshed out startup

--- MovieListAPI/Startup.cs
+++ MovieListAPI/Startup.cs
@@ -2,5 +2,34 @@
 {
     public class Startup
     {
+        public void Configure()
+        {
+            var builder = WebApplication.CreateBuilder();
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddHttpClient();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
     }
 }
