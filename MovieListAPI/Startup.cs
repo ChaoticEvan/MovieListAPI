c21b6817bd86dd526b945fb08c70be897b1784f9
Evan Voordeckers <evan.voordeckers@yahoo.com>
2023-02-13 16:41:21 -0500

    Added swaggergen config

--- MovieListAPI/Startup.cs
+++ MovieListAPI/Startup.cs
@@ -1,4 +1,6 @@
-﻿namespace MovieListAPI
+﻿using Microsoft.OpenApi.Models;
+
+namespace MovieListAPI
 {
     public class Startup
     {
@@ -11,9 +13,15 @@
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            { 
+                c.SwaggerDoc("v3", new OpenApiInfo
+                {
+                    Title = "GTrackAPI",
+                    Version = "v3"
+                });
+            });
             builder.Services.AddHttpClient();
-            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
